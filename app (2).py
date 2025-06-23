9a62d0862d2bf9335239bd6cc4cf3330a31c5bce
EmmanuelK <131205553+KieriseiyeEmmanuel@users.noreply.github.com>
2025-06-23 13:20:27 +0100

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import numpy_financial as npf
+import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 import seaborn as sns
